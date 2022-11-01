e5a2fb00a821bb7cab88999e598305c220c62ec2
Ubuntu <ubuntu@ip-172-31-89-132.ec2.internal>
2022-11-01 14:25:25 +0000

    Update file

--- mm_explicit_AddressOrder.c
+++ mm_explicit_AddressOrder.c
@@ -61,7 +61,6 @@ team_t team = {
 #define SUCC(bp)            ((char *)(bp) + WSIZE)
 
 
-static char *heap_listp = NULL;
 static char *root = NULL;
 
 // free block을 절대 주소값을 기준으로 삽입
@@ -200,15 +199,13 @@ static void *extend_heap(size_t words)
  */
 int mm_init(void)
 {
-    if((heap_listp = mem_sbrk(4 * WSIZE)) == (void *)-1) {
+    if((root = mem_sbrk(4 * WSIZE)) == (void *)-1) {
         return -1;
     }
-    PUT(heap_listp, 0);                             /* Alignment padding */
-    PUT(heap_listp + (1*WSIZE), PACK(DSIZE, 1));    /* Prologue header */
-    PUT(heap_listp + (2*WSIZE), PACK(DSIZE, 1));    /* Prologue footer */
-    PUT(heap_listp + (3*WSIZE), PACK(0, 1));        /* Epilogue header */
-    root = heap_listp;
-    heap_listp += (2*WSIZE);
+    PUT(root, 0);                             /* Alignment padding */
+    PUT(root + (1*WSIZE), PACK(DSIZE, 1));    /* Prologue header */
+    PUT(root + (2*WSIZE), PACK(DSIZE, 1));    /* Prologue footer */
+    PUT(root + (3*WSIZE), PACK(0, 1));        /* Epilogue header */
 
     if(extend_heap(CHUNKSIZE/WSIZE) == NULL) {
         return -1;
